648d625df81c8a9790557e78cab8f41f1df9d631
Saaayurii <relmontovro@gmail.com>
2024-08-05 21:00:15 +0300

    3 commit

--- src/app/(main)/search/page.tsx
+++ src/app/(main)/search/page.tsx
@@ -1,6 +1,7 @@
 import TrendsSidebar from "@/components/TrendsSidebar";
 import { Metadata } from "next";
 import SearchResults from "./SearchResults";
+import { Card } from "@/components/ui/card";
 
 interface PageProps {
   searchParams: { q: string };
@@ -19,11 +20,11 @@ export default function Page({ searchParams: { q } }: PageProps) {
   return (
     <main className="flex w-full min-w-0 gap-5">
       <div className="w-full min-w-0 space-y-5">
-        <div className="rounded-2xl bg-card p-5 shadow-sm">
+        <Card className="rounded-2xl bg-card p-5 shadow-sm">
           <h1 className="line-clamp-2 break-all text-center text-2xl font-bold">
             Результаты поиска по запросу &quot;{decodedQ}&quot;
           </h1>
-        </div>
+        </Card>
         <SearchResults query={q} />
       </div>
       <TrendsSidebar />
